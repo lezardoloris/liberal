e7e90901133b6caaa4c3a9fa36614586cdfd5416
Nicolas Paye <dev@nicolaspaye.fr>
2026-02-27 18:35:36 +0700

    fix: update fallback domain to nicoquipaie.co

--- liberal/src/lib/metadata.ts
+++ liberal/src/lib/metadata.ts
@@ -1,5 +1,5 @@
 export const SITE_URL =
-  process.env.NEXT_PUBLIC_APP_URL || 'https://nicolaspaye.fr';
+  process.env.NEXT_PUBLIC_APP_URL || 'https://nicoquipaie.co';
 export const SITE_NAME = "C'EST NICOLAS QUI PAYE";
 export const TWITTER_HANDLE = '@NicolasPaye_FR';
 export const DEFAULT_OG_IMAGE = `${SITE_URL}/og-default.png`;
